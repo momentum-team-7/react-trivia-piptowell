647163d3ffca5c285ebe6b71439ecf6335e27e6d
Tip Powell <powelltip@gmail.com>
2021-03-23 12:46:39 -0400

    rendering all categories

--- react-trivia/src/App.js
+++ react-trivia/src/App.js
@@ -7,8 +7,9 @@ function App() {
   
   useEffect(() => {
     console.log('useEffect runs', categories)
-    axios.get('https://opentdb.com/api.php?amount=10').then((response) => {
-    setCategories(response.data.results)
+    axios.get('https://opentdb.com/api_category.php').then((response) => {
+      console.log(response)
+    setCategories(response.data.trivia_categories)
     })
   }, [])
 
@@ -16,10 +17,10 @@ function App() {
 
   return (
     <div className='wrapper'>
-      <h1>Trivia, ya dangus</h1>
+      <h1>Trivia, ya</h1>
       <ul>
         {categories.map((category, idx) => (
-          <li key={idx}>{category.question}</li>
+          <li key={idx}>{category.name}</li>
         ))}
       </ul>
     </div>
