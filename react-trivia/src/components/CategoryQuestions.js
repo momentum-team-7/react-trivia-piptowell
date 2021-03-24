43ab65d4962c69298a63277f8adff390aebf186f
Tip Powell <powelltip@gmail.com>
2021-03-23 22:38:46 -0400

    got them questions rendering lets gooooooo bzz bzz

--- react-trivia/src/components/CategoryQuestions.js
+++ react-trivia/src/components/CategoryQuestions.js
@@ -8,21 +8,31 @@ export default function CategoryQuestions({ category, handleGoBack }) {
         axios
             .get(`https://opentdb.com/api.php?amount=10&category=${category.id}`)
             .then((response) => {
-                console.log(response)
-                // const data = response.data.map()
+                // console.log('RESPONSE:', response.data.results)
+                const data = response.data.results.map((dataByCategory) => ({
+                    category: dataByCategory.category,
+                    type: dataByCategory.type,
+                    difficulty: dataByCategory.difficulty,
+                    incorrect_answers: dataByCategory.incorrect_answers,
+                    question: dataByCategory.question,
+                    id: dataByCategory.id
+                }))
+            setCategoryQuestions(data)
             })
-    })
+    }, [category])
 
     return (
         <div>
-            <h3>{category.name} Questions</h3>
+            <h3>{category.name} Questions and also Questions</h3>
             <button
             className="pa0 bw0 bg-white blue pointer underline-hover"
             onClick={handleGoBack}>
             Back to other categories
             </button>
             <ul>
-
+                {categoryQuestions.map((dataByCategory) => (
+                <li key={dataByCategory.id}>{dataByCategory.question}</li>
+                ))}
             </ul>
         </div>
     )
